44f769c7d9859cef9f930d62bd8667eec5a42e93
Bill Doody <wdoody@yahoo.com>
2018-07-09 13:51:17 -0600

    added chatkit credentials

---
+++ index.js
@@ -0,0 +1,8 @@
+// test token provider endpoint from ChatKit
+const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/242b2a4f-9a36-4d4a-aa00-41187cedbeea/token"
+// instance locator
+const instanceLocator = "v1:us1:242b2a4f-9a36-4d4a-aa00-41187cedbeea"
+// roomId from Chatkit  */
+const roomId = 11284748
+// username from Chatkit
+const username = 'Bill'
